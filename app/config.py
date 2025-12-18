5bfed8ef601d5d2f7c2e7f67d0d33a4893989a3c
kgaudelli88@gmail.com <kgaudelli88@gmail.com>
2025-12-18 14:03:57 -0600

    Run create_tables.py successfully with correct PYTHONPATH

---
+++ app/config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CACHE_TYPE = 'SimpleCache'
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'mysql+mysqlconnector://root:Trinity2010@localhost/mechanic_shop')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI', 'sqlite:///:memory:')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'postgresql://final_project_8iq6_user:abXrH1mbcuYHnSnijpxHtnvT7teouRUq@dpg-d51h90mmcj7s73a4v2kg-a.oregon-postgres.render.com/final_project_8iq6')
